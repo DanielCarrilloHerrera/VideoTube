e736845080bfda81f72efffd2d03ab9b66e60f54
Daniel Carrillo Herrera <danielcarrillohcse@gmail.com>
2018-01-24 12:13:52 -0500

    Terminada la parte de la colocación del reproductor en la cantidad de segundos indicados por el usuario.

---
+++ app/src/main/java/daniel/com/videotube/MainActivity.java
@@ -0,0 +1,172 @@
+package daniel.com.videotube;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+import daniel.com.videotube.classes.Config;
+
+
+public class MainActivity extends YouTubeBaseActivity  implements YouTubePlayer.OnInitializedListener {
+
+    private static final int RECOVERY_REQUEST = 1;
+    private YouTubePlayer player;
+    private YouTubePlayerView youTubeView;
+    private MyPlayerStateChangeListener playerStateChangeListener;
+    private MyPlaybackEventListener playbackEventListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+        youTubeView.initialize(Config.YOUTUBE_API_KEY, this);
+
+        playerStateChangeListener = new MyPlayerStateChangeListener();
+        playbackEventListener = new MyPlaybackEventListener();
+
+        final EditText seekToText = (EditText) findViewById(R.id.seek_to_text);
+        Button seekToButton = (Button) findViewById(R.id.seek_to_button);
+        seekToButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int skipToSecs = Integer.valueOf(seekToText.getText().toString());
+                player.seekToMillis(skipToSecs * 1000);
+            }
+        });
+    }
+
+    @Override
+    public void onInitializationSuccess(Provider provider, YouTubePlayer player, boolean wasRestored) {
+        this.player = player;
+
+        if(!wasRestored){
+
+            player.setPlayerStateChangeListener(playerStateChangeListener);
+            player.setPlaybackEventListener(playbackEventListener);
+
+            if(!wasRestored){
+                player.cueVideo("QfDokqgrIZs");
+                //Plays https://www.youtube.com/watch?v=QfDokqgrIZs
+            }
+
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(Provider provider, YouTubeInitializationResult errorReason) {
+        if(errorReason.isUserRecoverableError()){
+            errorReason.getErrorDialog(this, RECOVERY_REQUEST).show();
+        } else {
+            String error = String.format(getString(R.string.player_error),
+                                        errorReason.toString());
+            Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == RECOVERY_REQUEST){
+            //Retry initialization if user performed a recovery action
+
+            getYouTubePlayerProvider().initialize(Config.YOUTUBE_API_KEY,
+                    this);
+        }
+    }
+
+    protected Provider getYouTubePlayerProvider(){
+
+        return youTubeView;
+    }
+
+    private void showMessage(String message){
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();;
+    }
+
+    private final class MyPlaybackEventListener
+            implements YouTubePlayer.PlaybackEventListener{
+
+        @Override
+        public void onPlaying() {
+            //Called when playback starts, either due to user action
+            //or call to play()
+            showMessage("Playing");
+        }
+
+        @Override
+        public void onPaused() {
+            //Called when playback is paused, either due to user action
+            //or call to pause()
+            showMessage("Paused");
+        }
+
+        @Override
+        public void onStopped() {
+            //Called when playback stops for a reason other than being paused
+            showMessage("Stopped");
+
+        }
+
+        @Override
+        public void onBuffering(boolean b) {
+            //Called when buffering starts or ends.
+        }
+
+        @Override
+        public void onSeekTo(int i) {
+            //Called when a jump in playback position occurs, either
+            //due to user srubbing or call to seekRelativeMillis() or seekToMillis()
+        }
+    }
+
+    private final class MyPlayerStateChangeListener
+            implements YouTubePlayer.PlayerStateChangeListener{
+
+        @Override
+        public void onLoading() {
+            //Called when the player is loading a video
+            //At this point, it's not ready to accept commands
+            // affecting playback such as play() or pause()
+        }
+
+        @Override
+        public void onLoaded(String s) {
+            //Called when a video is done loading.
+            //Playback methods such as play(), pause() or seekToMillis(int)
+            //may be called after this callback.
+        }
+
+        @Override
+        public void onAdStarted() {
+            //Called when playback of an advertisement starts.
+        }
+
+        @Override
+        public void onVideoStarted() {
+            //Called when playback of the video starts.
+
+        }
+
+        @Override
+        public void onVideoEnded() {
+            //Called when the video reaches its end.
+        }
+
+        @Override
+        public void onError(YouTubePlayer.ErrorReason errorReason) {
+            //Called when an error occurs.
+        }
+    }
+
+}
---
+++ app/src/main/java/daniel/com/videotube/classes/Config.java
@@ -0,0 +1,15 @@
+package daniel.com.videotube.classes;
+
+/**
+ * Created by daniel on 24/01/18.
+ */
+
+public class Config {
+
+    private Config(){
+
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyCPkgMrid5sVvgQ-eUVDI_PpHH7GggvJ9A";
+
+}
